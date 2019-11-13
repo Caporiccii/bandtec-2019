68f59e674f6d01e9b3aca49530f6acc0529e2a93
Marco David Coelho <marco.coelho@easynvest.com.br>
2019-11-13 15:26:33 -0300

    Filtra 5 processos

--- MonitorEasy/src/main/java/com/monitoreasy/Processos.java
+++ MonitorEasy/src/main/java/com/monitoreasy/Processos.java
@@ -5,16 +5,26 @@
  */
 package com.monitoreasy;
 
+
+import java.util.Arrays;
+import java.util.List;
 import org.apache.log4j.Logger;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OperatingSystem;
+import oshi.util.FormatUtil;
 
 public class Processos {
 
     SystemInfo sistemaInfo = new SystemInfo();
     HardwareAbstractionLayer hardwareAbstracao = sistemaInfo.getHardware();
+    OperatingSystem os = sistemaInfo.getOperatingSystem();
     String processoTotal;
+    String processos;
+    int n = 5;
     private final Logger logger;
 
     public Processos(Logger logger) {
@@ -33,4 +43,17 @@ public class Processos {
         }
         return processoTotal;
     }
+    public String getProcessos(GlobalMemory memoria){
+       List<OSProcess> proce = Arrays.asList(os.getProcesses(5, OperatingSystem.ProcessSort.CPU));
+        
+         processos = "   PID  %CPU %MEM       VSZ       RSS Name\n";
+       for (int i = 0; i < proce.size() && i < 5; i++) {
+           OSProcess p = proce.get(i);
+           processos += String.format(String.format(" %5d %5.1f %4.1f %9s %9s %s\n", p.getProcessID(),
+                   100d * (p.getKernelTime() + p.getUserTime()) / p.getUpTime(),
+                   100d * p.getResidentSetSize() / memoria.getTotal(), FormatUtil.formatBytes(p.getVirtualSize()),
+                   FormatUtil.formatBytes(p.getResidentSetSize()), p.getName()));
+       }
+       return processos;
+   }
 }
